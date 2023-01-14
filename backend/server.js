c1c7f70461d581e7305ff6510f63b47a59d59314
Amit Singh Rana <ranaa73000@gmail.com>
2023-01-14 14:52:22 +0530

    Connect to DB

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,20 @@ import dotenv from "dotenv";
 
 // Import from files
 import authRoute from "./routes/auth.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 // Configs / Setups / Middlewares
 dotenv.config({ path: "config/.env" }); // Env variable config
 app.use(express.json()); // Use JSON from requests as object in backend for node.js
+
+// Connect to the Database
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGOURI, (err) => {
+  if (err) console.log(err.message);
+  else console.log("DB Connected");
+});
 
 // Attach Routes
 app.use("/api/auth", authRoute);
