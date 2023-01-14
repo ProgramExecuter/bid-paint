f0655f7522989cf7ca7f51f379a4bcf61e93780b
Amit Singh Rana <ranaa73000@gmail.com>
2023-01-14 14:53:35 +0530

    added logic for logging in user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,8 +6,26 @@ const loginUser = (req, res) => {
 };
 
 // Logic for registering User
-const registerUser = (req, res) => {
-  res.status(200).json(newUser);
+const registerUser = async (req, res) => {
+  try {
+    // Check if username and password are present, if any not present, return error
+    if (!req.body.username || !req.body.password)
+      res
+        .status(400)
+        .json({ success: false, error: "Username and Password required" });
+
+    // Create new user
+    const newUser = new User(req.body);
+
+    // Save in DB
+    await newUser.save();
+
+    // Send back the response
+    res.status(201).json({ success: true, data: newUser });
+  } catch (err) {
+    // Send the error
+    res.status(500).json({ success: false, error: err.message });
+  }
 };
 
 export { loginUser, registerUser };
