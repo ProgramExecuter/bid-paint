d60585f263eddeaaea2dce2713310b593c660aae
Amit Singh Rana <ranaa73000@gmail.com>
2023-01-27 23:44:02 +0530

    Back to basic setup

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,31 +1,21 @@
+// Import packages
+import bcrypt from "bcryptjs";
+
+// Import files-functions
 import User from "../models/user.js";
 
-// Logic for logging-in User
+///////////////////////////
+///  LOGIN USER
+//////////////////////////
 const loginUser = (req, res) => {
   res.status(200).json("Login User");
 };
 
-// Logic for registering User
+///////////////////////////
+///  REGISTER USER
+//////////////////////////
 const registerUser = async (req, res) => {
-  try {
-    // Check if username and password are present, if any not present, return error
-    if (!req.body.username || !req.body.password)
-      res
-        .status(400)
-        .json({ success: false, error: "Username and Password required" });
-
-    // Create new user
-    const newUser = new User(req.body);
-
-    // Save in DB
-    await newUser.save();
-
-    // Send back the response
-    res.status(201).json({ success: true, data: newUser });
-  } catch (err) {
-    // Send the error
-    res.status(500).json({ success: false, error: err.message });
-  }
+  res.status(200).json("Signup User");
 };
 
 export { loginUser, registerUser };
