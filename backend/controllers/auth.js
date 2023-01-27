00b2703054120ed345124fee5c9050a0489fbd83
Amit Singh Rana <ranaa73000@gmail.com>
2023-01-28 00:18:35 +0530

    Added the logic for registering user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,8 +1,6 @@
-// Import packages
-import bcrypt from "bcryptjs";
-
 // Import files-functions
 import User from "../models/user.js";
+import { hashPassword, createJWT } from "../utils/utils.js";
 
 ///////////////////////////
 ///  LOGIN USER
@@ -14,8 +12,33 @@ const loginUser = (req, res) => {
 ///////////////////////////
 ///  REGISTER USER
 //////////////////////////
+// Steps as follows -
+//   1. Check if 'username' and 'password' are present
+//   2. Hash the password
+//   3. Create the new user and save it in DB
 const registerUser = async (req, res) => {
-  res.status(200).json("Signup User");
+  // Check if username and password are present
+  if (!req.body.username || !req.body.password)
+    return res.status(400).json({
+      success: false,
+      message: "Username and Password are required.",
+    });
+
+  // Hash the password
+  req.body.password = hashPassword(req.body.password);
+
+  // Create new User and save it in DB
+  const newUser = new User(req.body);
+  await newUser.save();
+
+  // Create a JsonWebToken for this login(signup), and return it
+  const token = createJWT({
+    _id: newUser._id,
+    username: newUser.username,
+  });
+
+  // Return the result
+  res.status(200).json({ success: true, token });
 };
 
 export { loginUser, registerUser };
---
+++ backend/utils/utils.js
@@ -0,0 +1,15 @@
+// Import packages
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Hash the password and return it
+export const hashPassword = (pass) => {
+  const hashedPass = bcrypt.hashSync(pass, 10);
+  return hashedPass;
+};
+
+// Create a Json Web Token and return it
+export const createJWT = (data) => {
+  const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
+  return token;
+};
