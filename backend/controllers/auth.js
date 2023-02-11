559a1a27c54297c1f42410e842b7fd264da21192
Amit Singh Rana <ranaa73000@gmail.com>
2023-02-11 16:34:34 +0530

    Better comments and some tweaks

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,20 +2,16 @@
 import User from "../models/user.js";
 import { hashPassword, createJWT } from "../utils/utils.js";
 
-///////////////////////////
-///  LOGIN USER
-//////////////////////////
+/**
+ * * LOGIN USER
+ */
 const loginUser = (req, res) => {
   res.status(200).json("Login User");
 };
 
-///////////////////////////
-///  REGISTER USER
-//////////////////////////
-// Steps as follows -
-//   1. Check if 'username' and 'password' are present
-//   2. Hash the password
-//   3. Create the new user and save it in DB
+/**
+ * * REGISTER USER
+ */
 const registerUser = async (req, res) => {
   // Check if username and password are present
   if (!req.body.username || !req.body.password)
@@ -31,17 +27,14 @@ const registerUser = async (req, res) => {
   const newUser = new User(req.body);
   await newUser.save();
 
-  // Create a JsonWebToken for this login(signup), and return it
+  // Create a JsonWebToken for this login(signup)
   const token = createJWT(
-    {
-      _id: newUser._id,
-      username: newUser.username,
-    },
+    { _id: newUser._id, username: newUser.username },
     { expiresIn: "3d" }
   );
 
   // Return the result
-  res.status(200).json({ success: true, token });
+  res.status(200).json({ success: true, jwt: token });
 };
 
 export { loginUser, registerUser };
