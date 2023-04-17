c9a535b36980c238101110bfa8ea7d1025ee3e36
Amit Singh Rana <ranaa73000@gmail.com>
2023-04-17 22:16:47 +0530

    Setup basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+// Import packages
+const express = require("express");
+
+const app = express();
+
+// Server setup
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
