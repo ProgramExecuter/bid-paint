33528e25419d10b1dc25fa671b80ebb5f9c90d5f
Amit Singh Rana <ranaa73000@gmail.com>
2023-04-20 14:10:22 +0530

    Implemented basic logic on 'GET /user/:id' route

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,17 @@
-export const getParticularUser = (req, res) => {
-  res.status(200).json("GET /user/:id");
+// Import files-functions
+import User from "../models/user.js";
+
+export const getParticularUser = async (req, res) => {
+  try {
+    const foundUser = await User.findById(req.params.id);
+
+    if (!foundUser) throw Error();
+
+    res.status(200).json(foundUser);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(404);
+  }
 };
 
 export const userSignup = (req, res) => {
