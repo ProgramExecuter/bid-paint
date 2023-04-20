a5430a0267fd104438ea8fbf9b50b77e64b9f3e9
Amit Singh Rana <ranaa73000@gmail.com>
2023-04-20 19:54:19 +0530

    Implemented basic logic on 'POST /user/signup' route

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,9 @@
 import User from "../models/user.js";
 
 export const getParticularUser = async (req, res) => {
+  console.log(req.params.id);
   try {
+    console.log("Hello");
     const foundUser = await User.findById(req.params.id);
 
     if (!foundUser) throw Error();
@@ -14,8 +16,16 @@ export const getParticularUser = async (req, res) => {
   }
 };
 
-export const userSignup = (req, res) => {
-  res.status(200).json("POST /user/signup");
+export const userSignup = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(200).json(newUser);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json(err.message);
+  }
 };
 
 export const userLogin = (req, res) => {
